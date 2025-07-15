574e688d02fb6f37861ea421736bdfd4ae4e5def
WMPShashini <shashiniwmp.22@uom.lk>
2025-07-16 02:34:39 +0530

    Update: with checkout page

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import roomsRoutes from './routes/RoomsRoutes.js';
+import receptionRoutes from './routes/ReceptionRoutes.js';
+import settingsRoutes from './routes/SettingsRoutes.js';
+import salesRoutes from './routes/SalesRoutes.js';
+import transactionRoutes from './routes/TransactionRoutes.js';
+import stockRoutes from './routes/StockRoutes.js';
+import checkoutRoutes from './routes/CheckoutRoutes.js';
 
 dotenv.config();
-
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -15,26 +22,17 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-const roomsRoutes = require('./routes/RoomsRoutes'); 
-app.use('/api/rooms', roomsRoutes); // will now correctly resolve to /api/rooms
-  
-const receptionRoutes = require('./routes/ReceptionRoutes');
+app.use('/api/rooms', roomsRoutes);
 app.use('/api/reception', receptionRoutes);
-
-const settingsRoutes = require('./routes/SettingsRoutes');
 app.use('/api/settings', settingsRoutes);
-app.use("/uploads", express.static("uploads")); // Serve uploaded logos
-
-const salesRoutes = require('./routes/SalesRoutes');
 app.use('/api/sales', salesRoutes);
-
-const transactionRoutes = require("./routes/TransactionRoutes");
-app.use("/api/transactions", transactionRoutes);
-
-const stockRoutes = require("./routes/StockRoutes");
-app.use("/api/stocks", stockRoutes);
+app.use('/api/transactions', transactionRoutes);
+app.use('/api/stocks', stockRoutes);
+app.use('/api/checkouts', checkoutRoutes);
+app.use("/uploads", express.static("uploads"));
 
 const PORT = process.env.PORT || 5003;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
