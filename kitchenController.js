f377181706dd94a90b569197c371b26c70f53626
Jananji-01 <jananji505@gmail.com>
2025-07-15 23:38:01 +0530

    Add files via upload

---
+++ kitchenController.js
@@ -0,0 +1,32 @@
+const KitchenOrder = require('../models/KitchenOrder');
+
+exports.createOrder = async (req, res) => {
+  try {
+    const order = await KitchenOrder.create(req.body);
+    res.status(201).json(order);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.getOrders = async (req, res) => {
+  try {
+    const orders = await KitchenOrder.find();
+    res.json(orders);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.updateOrderStatus = async (req, res) => {
+  try {
+    const order = await KitchenOrder.findByIdAndUpdate(
+      req.params.id,
+      { status: req.body.status },
+      { new: true }
+    );
+    res.json(order);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
