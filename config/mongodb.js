ad3063e980cfabe81c2aef9507bdd490e1dac846
Abdulla <abdullahishak3034@gmail.com>
2025-07-19 08:42:34 +0530

    change db

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,13 +1,10 @@
 import mongoose from "mongoose";
 
-const connectDB = async ()=>{
-    mongoose.connection.on(`connected`, ()=>console.log("  Connect Database ")) ;
+const connectDB = async () => {
+    mongoose.connection.on(`connected`, () => console.log("Database Connected"));
 
-
-    await mongoose.connect(`${ 
-process.env.MONGODB_URI }/mern-auth`  //  `mongodb://localhost:27017/mern-auth` , 
-
-);
+    // Change MONGODB_URI to MONGO_URI to match your .env file
+    await mongoose.connect(`${process.env.MONGO_URI}/mern-auth`);
 };
 
 export default connectDB;
